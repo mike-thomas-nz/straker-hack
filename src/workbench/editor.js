5908eec932ae444894e47d3a57dfe053ad73dbdb
Parmpreet Singh <param@strakertranslations.com>
2019-05-18 19:54:29 +1200

    Adding machine translation

--- src/workbench/editor.js
+++ src/workbench/editor.js
@@ -1,6 +1,8 @@
 import $ from "jquery";
 import * as player from "./videoplayer";
-let srtSrc = '/resources/subs/animation/english.srt'
+import Papa from "papaparse";
+let srtSrc = '/resources/subs/animation/english__Chinese.csv'
+// let srtSrc = '/resources/subs/animation/english.srt'
 var PF_SRT = function() {
   // eslint-disable-next-line
   var pattern = /(\d+)\n([\d:.]+)\s+-{2}\>\s+([\d:.]+)\n([\s\S]*?(?=\n{2}|$))/gm;
@@ -43,10 +45,12 @@ const loadSegments= () => {
     .done(function(text) {
       try {
         //Array with {line, startTime, endTime, text}
-        var result = PF_SRT.parse(text);
+        // var result = PF_SRT.parse(text);
+        let result = Papa.parse(text).data
 
         $.each(result, function(index, value) {
-          $("#table_div").append("<tr id=caption_"+value.line+"><td>"+value.line+"</td><td>" + value.startTime + " - "+ value.endTime + "</td><td>" + value.text + "</td><td><div class='editable_seg' contenteditable=true></div></div></td><td><button class='save btn btn-success btn-sm'>Save</button></td></td></tr>");
+          $("#table_div").append("<tr id=caption_"+value[0]+"><td>"+value[0]+"</td><td>" + value[1] + "</td><td>" + value[2] + "</td><td><div class='editable_seg' contenteditable=true>"+value[3]+"</div></td><td><button class='save btn btn-success btn-sm'>Save</button></td></tr>");
+          // $("#table_div").append("<tr id=caption_"+value.line+"><td>"+value.line+"</td><td>" + value.startTime + " - "+ value.endTime + "</td><td>" + value.text + "</td><td><div class='editable_seg' contenteditable=true></div></div></td><td><button class='save btn btn-success btn-sm'>Save</button></td></td></tr>");
         });
       } catch (e) {
         // console.log(e);
